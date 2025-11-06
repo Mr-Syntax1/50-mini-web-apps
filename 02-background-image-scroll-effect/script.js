091e7a88521deaeb72faeab0824b903476160183
Mr. Syntax <maam98617@gmail.com>
2025-11-06 15:12:20 +0330

    Add files via upload

---
+++ 02-background-image-scroll-effect/script.js
@@ -0,0 +1,12 @@
+const bgImageEl = document.querySelector('#bg-image');
+
+window.addEventListener('scroll', () => {
+    updateImage();
+})
+
+
+function updateImage() {
+   // bgImageEl.style.opacity = 1 - window.pageYOffset / 1100;
+    if (window.innerWidth <= 800) return;
+    bgImageEl.style.backgroundSize = 100 + window.pageYOffset / 12 + "%";
+}
