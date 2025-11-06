1741442b8f91c72789a8bf874c1236a0764799f2
Mr. Syntax <maam98617@gmail.com>
2025-11-06 15:16:16 +0330

    Update script.js

--- 02-background-image-scroll-effect/script.js
+++ 02-background-image-scroll-effect/script.js
@@ -7,6 +7,6 @@ window.addEventListener('scroll', () => {
 
 function updateImage() {
    // bgImageEl.style.opacity = 1 - window.pageYOffset / 1100;
-    if (window.innerWidth <= 800) return;
     bgImageEl.style.backgroundSize = 100 + window.pageYOffset / 12 + "%";
+
 }
